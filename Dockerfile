fd44e67dd039ec8fbe37fb6e3e13e788e41dcb6a
John Mihalic <2854333+mezz64@users.noreply.github.com>
2018-11-19 18:38:23 +0000

    Updates

--- Dockerfile
+++ Dockerfile
@@ -46,6 +46,8 @@ RUN git clone https://github.com/merbanan/rtl_433.git \
 # 
 # Use this variable when creating a container to specify the MQTT broker host.
 ENV MQTT_HOST=""
+ENV MQTT_USER=""
+ENV MQTT_PASS=""
 
 #
 # When running a container this script will be executed
--- rtl2mqtt.sh
+++ rtl2mqtt.sh
@@ -11,15 +11,17 @@
 export LANG=C
 PATH="/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin"
 
-#
-# Start the listener and enter an endless loop
-#
-/usr/local/bin/rtl_433 -F json |  while read line
-do
-# Log to file if file exists.
-# Create file with touch /tmp/rtl_433.log if logging is needed
-  [ -w /tmp/rtl_433.log ] && echo $line >> rtl_433.log
+# #
+# # Start the listener and enter an endless loop
+# #
+# /usr/local/bin/rtl_433 -F json |  while read line
+# do
+# # Log to file if file exists.
+# # Create file with touch /tmp/rtl_433.log if logging is needed
+#   [ -w /tmp/rtl_433.log ] && echo $line >> rtl_433.log
 
-# Raw message to MQTT
-  echo $line  | /usr/bin/mosquitto_pub -h $MQTT_HOST -i RTL_433 -l -t "RTL_433/Raw"
-done
+# # Raw message to MQTT
+#   echo $line  | /usr/bin/mosquitto_pub -h $MQTT_HOST -u $MQTT_USER -P $MQTT_PASS -i RTL_433 -l -t "house/rtl433/raw"
+# done
+
+/usr/local/bin/rtl_433 -F json -U | /usr/bin/mosquitto_pub -h $MQTT_HOST -u $MQTT_USER -P $MQTT_PASS -i RTL_433 -l -t "house/rtl433/raw"
